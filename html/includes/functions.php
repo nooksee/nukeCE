2401fba476bae4a9324ce9131135b701aa809b9d
Kevin <admin@nooksee.com>
2012-04-01 18:15:52 -0700

    Added truncate function to system functions.

--- html/includes/functions.php
+++ html/includes/functions.php
@@ -1291,4 +1291,15 @@ function nuke_mail_batch($array_recipients){
     return $recipients;
 }
 
+// Original PHP code by Chirp Internet: www.chirp.com.au
+// Please acknowledge use of this code by including this header.
+function truncate($string, $limit, $break=" ", $pad="...") {
+    // return with no change if string is shorter than $limit
+    if(strlen($string) <= $limit) return $string; $string = substr($string, 0, $limit);
+    if(false !== ($breakpoint = strrpos($string, $break))) {
+        $string = substr($string, 0, $breakpoint);
+    }
+    return $string . $pad;
+}
+
 ?>
